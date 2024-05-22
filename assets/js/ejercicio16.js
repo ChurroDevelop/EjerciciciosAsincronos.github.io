5514a168590ce82894081591de520863e332f177
Churro Develop <jhonfredy2325@gmail.com>
2024-05-22 15:45:11 -0500

    Se termino el taller

--- assets/js/ejercicio16.js
+++ assets/js/ejercicio16.js
@@ -5,6 +5,7 @@ y mostrar el resultado final.
 */
 
 // Se crea una nueva promesa
+let suma = 0
 new Promise((resolve) => {
     setTimeout(() => {
         resolve(5) // El valor inicial que devolvera la promesa como respuesta aceptada sera 5
@@ -14,7 +15,8 @@ new Promise((resolve) => {
         return new Promise((resolve) => { // se crea una nueva promesa que retornara un a condicion
             setTimeout(() => {
                 console.log(`Numero inicial: ${respuesta}`) // imprime el valor inicial de la promesa
-                resolve(respuesta * 2); // El resolve de la nueva promesa que se retornda, tomara el valor de la respuesta de la primer promesa
+                suma += respuesta * 2; // Suma en cada promesa 
+                resolve(respuesta * 2) // El resolve de la nueva promesa que se retornda, tomara el valor de la respuesta de la primer promesa
                                         // y asi sucesivamente en cada .then de las promesas
             }, 3000)
         })
@@ -23,6 +25,7 @@ new Promise((resolve) => {
         return new Promise((resolve) => {
             setTimeout(() => {
                 console.log(`Numero despues de la primer promesa encadenada: ${respuesta}`)
+                suma += respuesta * 2;
                 resolve(respuesta * 2)
             }, 3000)
         })
@@ -31,6 +34,7 @@ new Promise((resolve) => {
         return new Promise((resolve) => {
             setTimeout(() => {
                 console.log(`Numero despues de la segunda promesa encadenada: ${respuesta}`)
+                suma += respuesta * 2;
                 resolve(respuesta * 2)
             }, 3000)
         })
@@ -38,6 +42,7 @@ new Promise((resolve) => {
     .then((respuesta) => {
         setTimeout(() => {
             console.log(`Numero al final de las promesas encadenadas \n Se multiplico 3 veces por 2: ${respuesta}`); // Imprime el resultado final de la promesa
+            console.log(`La suma de las promesas encadenadas es ${suma}`); 
         }, 3000)
     })
 
